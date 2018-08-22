b02caeb15e95b69d5ad77ae22655ce71da4d7aed
Ferdinand Espiritu <freddie.espiritu@yahoo.com>
2018-08-21 20:09:01 -0700

    Added SelectedIndicator component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import React, { Component } from "react";
 import "./App.css";
 import LoginForm from "./components/common/loginForm";
 import "bootstrap/dist/css/bootstrap.css";
+import SelectedIndicator from "./components/common/selectedIndicator";
 
+const items = [
+  { id: 1, label: "Brown bag 1" },
+  { id: 2, label: "Brown bag 2" },
+  { id: 3, label: "Brown bag 3" },
+  { id: 4, label: "Brown bag 4" }
+];
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
   render() {
     return (
       <div className="App">
@@ -12,6 +22,8 @@ class App extends Component {
           <h1>Vidly</h1>
         </header>
         <LoginForm />
+        <hr />
+        <SelectedIndicator items={items} />
       </div>
     );
   }
